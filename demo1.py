78fab8c1ad80794744560ea752faa3eeaa46aff4
Chelike Nikhil <100200794+nikhil32047@users.noreply.github.com>
2025-07-25 20:04:04 +0530

    Update demo1.py

--- demo1.py
+++ demo1.py
@@ -1,7 +1,27 @@
+#Simple FastAPI application
+from fastapi import FastAPI
+app=FastAPI()
+@app.get("/T1")
+async def TestCase1():
+    return {"Data":"Welcome to FastAPI Development for AI/ML application"}
+
+
 #path parameters
 from fastapi import FastAPI
 app=FastAPI()
 @app.get("/T2")
 async def TestCase1(name:str):
     return {"name":name}
+
+
+
+#query parameters(combination of path parameter)
+from fastapi import FastAPI
+app=FastAPI()
+@app.get("/T3")
+async def TestCase1(number:int,name:str):
+    return {"number":number,"name":name}
+
+
+
 
